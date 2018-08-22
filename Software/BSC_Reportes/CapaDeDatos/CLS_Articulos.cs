f2cfa25c23da37915fa0670e8dd1fe69670fd7cf
Juan Alvaro Esquivel Aguilar <iscjaea@gmail.com>
2018-08-22 17:23:07 -0600

    Subir Programas

---
+++ Software/BSC_Reportes/CapaDeDatos/CLS_Articulos.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapadeDatos
+{
+    public class CLS_Articulos : ConexionBase
+    {
+        public string ArticuloCodigo { get; set; }
+        public string ArticuloDescripcion { get; set; }
+
+        public void MtdInsertarArticulo()
+        {
+            TipoDato _dato = new TipoDato();
+            Exito = true;
+            try
+            {
+                _conexionC.NombreProcedimiento = "SP_BSC_ArticulosInsert";
+
+                _dato.CadenaTexto = ArticuloCodigo;
+                _conexionC.agregarParametro(EnumTipoDato.CadenaTexto, _dato, "ArticuloCodigo");
+                _dato.CadenaTexto = ArticuloDescripcion;
+                _conexionC.agregarParametro(EnumTipoDato.CadenaTexto, _dato, "ArticuloDescripcion");
+                _conexionC.EjecutarDataset();
+
+                if (_conexionC.Exito)
+                {
+                    Datos = _conexionC.Datos;
+                }
+                else
+                {
+                    Mensaje = _conexionC.Mensaje;
+                    Exito = false;
+                }
+            }
+            catch (Exception e)
+            {
+                Mensaje = e.Message;
+                Exito = false;
+            }
+        }
+        public void MtdActualizarArticulo()
+        {
+            TipoDato _dato = new TipoDato();
+            Exito = true;
+            try
+            {
+                _conexionC.NombreProcedimiento = "SP_BSC_ArticuloUpdate";
+
+                _dato.CadenaTexto = ArticuloCodigo;
+                _conexionC.agregarParametro(EnumTipoDato.CadenaTexto, _dato, "ArticuloCodigo");
+                _dato.CadenaTexto = ArticuloDescripcion;
+                _conexionC.agregarParametro(EnumTipoDato.CadenaTexto, _dato, "ArticuloDescripcion");
+
+                _conexionC.EjecutarDataset();
+
+                if (_conexionC.Exito)
+                {
+                    Datos = _conexionC.Datos;
+                }
+                else
+                {
+                    Mensaje = _conexionC.Mensaje;
+                    Exito = false;
+                }
+            }
+            catch (Exception e)
+            {
+                Mensaje = e.Message;
+                Exito = false;
+            }
+        }
+
+
+    }
+}
