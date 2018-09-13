df42e241642ef93bbc2c7c278369da01b093e9ad
George O <geoorge191@hotmail.com>
2018-09-13 18:54:18 -0500

    script server centro
    Prototipo procedimiento almacenado Server_Centro

---
+++ Scripts/Sucursales/ProcedimientosAlmacenados/SP_BSC_CancelacionArticulo_General.sql
@@ -0,0 +1,58 @@
+use Server_Centro
+GO
+-- ================================================
+-- Template generated from Template Explorer using:
+-- Create Procedure (New Menu).SQL
+--
+-- Use the Specify Values for Template Parameters 
+-- command (Ctrl-Shift-M) to fill in the parameter 
+-- values below.
+--
+-- This block of comments will not be included in
+-- the definition of the procedure.
+-- ================================================
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+IF  EXISTS (SELECT * FROM SYS.OBJECTS WHERE TYPE = 'P' AND NAME = 'SP_BSC_CancelacionArticulo_General')
+DROP PROCEDURE SP_BSC_CancelacionArticulo_General
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE SP_BSC_CancelacionArticulo_General
+	-- Add the parameters for the stored procedure here
+	@CancelacionId bigint,
+	@CajaId decimal(11, 0),
+	@CancelacionArticuloUltimoIde bigint,
+	@TicketId bigint,
+	@ArticuloCodigo char(40),
+	@CancelacionArticuloPrecio money,
+	@CancelacionArticuloCantidad bigint,
+	@CancelacionArticuloSubtotal money,
+	@CancelacionArticuloIva money,
+	@CancelacionArticuloTotalLinea money
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	declare @Existe int
+	declare @mensaje varchar(50)
+	
+	select @Existe = count(CancelacionId) from CancelacionArticulo a where (a.CancelacionId=@CancelacionId and a.CajaId=@CajaId)
+	if @Existe>0
+			select 'Ya existe esta CancelacionArticulo '+@CancelacionId;
+		else
+			 INSERT INTO CancelacionArticulo
+                         (CancelacionId, CajaId, CancelacionArticuloUltimoIde, TicketId, ArticuloCodigo, CancelacionArticuloPrecio, CancelacionArticuloCantidad, CancelacionArticuloSubtotal, CancelacionArticuloIva, CancelacionArticuloTotalLinea, 
+                         FechaInsert)
+				VALUES        (@CancelacionId,@CajaId,@CancelacionArticuloUltimoIde,@TicketId,@ArticuloCodigo,@CancelacionArticuloPrecio,@CancelacionArticuloCantidad,@CancelacionArticuloSubtotal,@CancelacionArticuloIva,@CancelacionArticuloTotalLinea,
+										  GETDATE());
+END
+GO
---
+++ Scripts/Sucursales/ProcedimientosAlmacenados/SP_BSC_Cancelacion_General.sql
@@ -0,0 +1,62 @@
+use Server_Centro
+GO
+-- ================================================
+-- Template generated from Template Explorer using:
+-- Create Procedure (New Menu).SQL
+--
+-- Use the Specify Values for Template Parameters 
+-- command (Ctrl-Shift-M) to fill in the parameter 
+-- values below.
+--
+-- This block of comments will not be included in
+-- the definition of the procedure.
+-- ================================================
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+IF  EXISTS (SELECT * FROM SYS.OBJECTS WHERE TYPE = 'P' AND NAME = 'SP_BSC_Cancelacion_General')
+DROP PROCEDURE SP_BSC_Cancelacion_General
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE SP_BSC_Cancelacion_General
+	-- Add the parameters for the stored procedure here
+	@CancelacionId bigint,
+	@CajaId decimal(11, 0),
+	@TicketId bigint,
+	@UsuarioId decimal(11, 0),
+	@CancelacionFecha datetime,
+	@CancelacionSubtotal0 money,
+	@CancelacionSubtotal16 money,
+	@CancelacionIva money,
+	@CancelacionTotal money,
+	@CancelacionAsignadoCorte bit,
+	@CorteZId bigint,
+	@CancelacionesTotal bit,
+	@TicketMayoreoId bigint,
+	@FechaInsert datetime
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	declare @Existe int
+	declare @mensaje varchar(50)
+	
+	select @Existe = count(CancelacionId) from Cancelacion a where (a.CancelacionId=@CancelacionId and a.CajaId=@CajaId)
+	if @Existe>0
+			select 'Ya existe esta cancelancion '+@CancelacionId;
+		else
+			 INSERT INTO Cancelacion
+                         (CancelacionId, CajaId, TicketId, UsuarioId, CancelacionFecha, CancelacionSubtotal0, CancelacionSubtotal16, CancelacionIva, CancelacionTotal, CancelacionAsignadoCorte, CorteZId, CancelacionesTotal, TicketMayoreoId, 
+                         FechaInsert)
+			VALUES        (@CancelacionId,@CajaId,@TicketId,@UsuarioId,@CancelacionFecha,@CancelacionSubtotal0,@CancelacionSubtotal16,@CancelacionIva,@CancelacionTotal,@CancelacionAsignadoCorte,@CorteZId,@CancelacionesTotal,@TicketMayoreoId,
+									  GETDATE());
+END
+GO
