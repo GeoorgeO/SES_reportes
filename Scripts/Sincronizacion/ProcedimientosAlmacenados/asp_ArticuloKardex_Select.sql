48edb14fbdbe78c55d2063d7ebd06a0ad1f1471b
George O <geoorge191@hotmail.com>
2018-09-21 19:09:26 -0500

    avance
    Procedimiento select sucursal

---
+++ Scripts/Sincronizacion/ProcedimientosAlmacenados/asp_ArticuloKardex_Select.sql
@@ -0,0 +1,39 @@
+-- ================================================
+-- Template generated from Template Explorer using:
+-- Create Procedure (New Menu).SQL
+--
+-- Use the Specify Values for Template Parameters 
+-- command (Ctrl-Shift-M) to fill in the parameter 
+-- values below.
+--
+-- This block of comments will not be included in
+-- the definition of the procedure.
+-- ================================================
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+IF  EXISTS (SELECT * FROM SYS.OBJECTS WHERE TYPE = 'P' AND NAME = 'asp_ArticuloKardex_Select')
+DROP PROCEDURE asp_ArticuloKardex_Select
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE asp_ArticuloKardex_Select
+	-- Add the parameters for the stored procedure here
+	@FechaInicio varchar(20),
+	@FechaFin varchar(20)
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	SELECT        ArticuloCodigo,ArticuloCantidad, ArticuloUltimoCosto,0, ArticuloFecha
+FROM            Articulo
+WHERE        (ArticuloFechaUltimoCosto BETWEEN @FechaInicio AND @FechaFin)
+END
+GO
