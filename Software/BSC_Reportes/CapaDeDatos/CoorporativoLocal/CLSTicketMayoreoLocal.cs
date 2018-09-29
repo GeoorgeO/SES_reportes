21de6252e8f894910f13a61194f390565bf36802
George O <geoorge191@hotmail.com>
2018-09-28 20:58:14 -0500

    capadedatos

--- Software/BSC_Reportes/CapaDeDatos/CoorporativoLocal/CLSTicketMayoreoLocal.cs
+++ Software/BSC_Reportes/CapaDeDatos/CoorporativoLocal/CLSTicketMayoreoLocal.cs
@@ -12,13 +12,13 @@ namespace CapaDeDatos
         public string FechaFin { get; set; }
         public string FechaInicio { get; set; }
 
-        public void MtdSeleccionarCancelacion()
+        public void MtdSeleccionarTicketMayoreo()
         {
             TipoDato _dato = new TipoDato();
             Exito = true;
             try
             {
-                _conexion.NombreProcedimiento = "SP_BSC_Centro_ActualizaTicketMayoreoLocal_Select";
+                _conexion.NombreProcedimiento = "asp_TicketMayoreo_Select";
                 _dato.CadenaTexto = FechaInicio;
                 _conexion.agregarParametro(EnumTipoDato.CadenaTexto, _dato, "FechaInicio");
                 _dato.CadenaTexto = FechaFin;
