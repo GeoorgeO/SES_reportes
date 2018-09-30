486346cade2168a6100c9a6f325beca666a99d4c
Juan Alvaro Esquivel Aguilar <iscjaea@gmail.com>
2018-09-29 23:22:01 -0500

    Cambio BSC

--- Scripts/Cooporativo/Tablas/BSC_CortesZRecibosDetalles_CreateTable.sql
+++ Scripts/Cooporativo/Tablas/BSC_CortesZRecibosDetalles_CreateTable.sql
@@ -17,15 +17,4 @@ ELSE
 			[CortesZRecibosId] ASC
 		)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 		) ON [PRIMARY]
-
-		
-
-		ALTER TABLE [dbo].[CortesZRecibosDetalles]  WITH CHECK ADD  CONSTRAINT [FK_CortesZRecibosDetalles_CortesZRecibos] FOREIGN KEY([CortesZRecibosId])
-		REFERENCES [dbo].[CortesZRecibos] ([CortesZRecibosId])
-		
-
-		ALTER TABLE [dbo].[CortesZRecibosDetalles] CHECK CONSTRAINT [FK_CortesZRecibosDetalles_CortesZRecibos]
-		
-		
-
 	end
