188fc09b0df878ee2b626e6d1e4d59271ba23f7b
Juan Alvaro Esquivel Aguilar <iscjaea@gmail.com>
2018-10-30 16:16:59 -0600

    Cambios

--- Software/BSC_Reportes/BSC-Reportes/Formularios/Frm_ReportePedidos.cs
+++ Software/BSC_Reportes/BSC-Reportes/Formularios/Frm_ReportePedidos.cs
@@ -64,5 +64,121 @@ namespace BSC_Reportes
             rdbTipo.SelectedIndex = 0;
             dtgVentaExistencia.DataSource = null;
         }
+
+        private void rdbTipo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(rdbTipo.SelectedIndex==0)
+            {
+                DetalladoGrids();
+                chkVentas.Enabled = true;
+                chkExistencia.Enabled = true;
+                chkVentas.Checked = true;
+                chkExistencia.Checked = true;
+            }
+            else
+            {
+                ConcentradoGrids();
+                chkVentas.Enabled = false;
+                chkExistencia.Enabled = false;
+                chkVentas.Checked = false;
+                chkExistencia.Checked = false;
+            }
+        }
+
+        private void DetalladoGrids()
+        {
+            ColumnsVentas(true);
+            ColumnsExistencia(true);
+            ColumnsBands(true);
+        }
+
+        private void ConcentradoGrids()
+        {
+            ColumnsVentas(false);
+            ColumnsExistencia(false);
+            ColumnsBands(false);
+        }
+
+        private void ColumnsBands(Boolean Mostrar)
+        {
+            gridBand2.Visible = Mostrar;
+            gridBand3.Visible = Mostrar;
+            gridBand4.Visible = Mostrar;
+            gridBand5.Visible = Mostrar;
+            gridBand6.Visible = Mostrar;
+            gridBand7.Visible = Mostrar;
+            gridBand8.Visible = Mostrar;
+            gridBand9.Visible = Mostrar;
+            gridBand10.Visible = Mostrar;
+            gridBand11.Visible = Mostrar;
+            gridBand12.Visible = Mostrar;
+            gridBand13.Visible = Mostrar;
+        }
+
+        private void ColumnsExistencia(Boolean Mostrar)
+        {
+            gridColumn4.Visible = Mostrar;
+            gridColumn6.Visible = Mostrar;
+            gridColumn8.Visible = Mostrar;
+            gridColumn10.Visible = Mostrar;
+            gridColumn12.Visible = Mostrar;
+            gridColumn14.Visible = Mostrar;
+            gridColumn16.Visible = Mostrar;
+            gridColumn18.Visible = Mostrar;
+            gridColumn20.Visible = Mostrar;
+            gridColumn22.Visible = Mostrar;
+            gridColumn24.Visible = Mostrar;
+            gridColumn26.Visible = Mostrar;
+        }
+
+        private void ColumnsVentas(Boolean Mostrar)
+        {
+            gridColumn3.Visible = Mostrar;
+            gridColumn5.Visible = Mostrar;
+            gridColumn7.Visible = Mostrar;
+            gridColumn9.Visible = Mostrar;
+            gridColumn11.Visible = Mostrar;
+            gridColumn13.Visible = Mostrar;
+            gridColumn15.Visible = Mostrar;
+            gridColumn17.Visible = Mostrar;
+            gridColumn19.Visible = Mostrar;
+            gridColumn21.Visible = Mostrar;
+            gridColumn23.Visible = Mostrar;
+            gridColumn25.Visible = Mostrar;
+        }
+
+        private void chkVentas_CheckedChanged(object sender, EventArgs e)
+        {
+            if(chkVentas.Checked==true)
+            {
+                ColumnsBands(true);
+                ColumnsVentas(true);
+            }
+            else if(chkVentas.Checked == false)
+            {
+                ColumnsVentas(false);
+            }
+            if(chkVentas.Checked == false && chkExistencia.Checked == false)
+            {
+                ColumnsBands(false);
+            }
+        }
+
+        private void chkExistencia_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkExistencia.Checked == true)
+            {
+                ColumnsBands(true);
+                ColumnsExistencia(true);
+            }
+            else if (chkExistencia.Checked == false)
+            {
+                ColumnsExistencia(false);
+            }
+            if (chkVentas.Checked == false && chkExistencia.Checked == false)
+            {
+                ColumnsBands(false);
+            }
+        }
     }
 }
